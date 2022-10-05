96c96f3da4c8d63ff6d3a38effd99299c52c9fc0
Kaylene <carnavos@hawaii.edu>
2022-10-04 15:38:14 -1000

    Part 7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    var currentIndex = 0
+    
+  
+   var favoriteFoodsArray = ["Pasta", "Sushi", "'Ulu", "Gyros", "Beyond Burgers"]
+    
+    
+    
+    
+    //view.did.load = whatever we want set when the app first opens!!!!!!!!!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        topLabel.text = "My Favorite Foods"
+        
+        bottomLabel.text = favoriteFoodsArray[currentIndex]
+        
+        
+        
     }
 
 
